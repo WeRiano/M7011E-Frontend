c3cffbc3bd9dc12d8875b42629bd4c1c49a0feb6
WeRiano <williamfors13@gmail.com>
2022-01-04 02:02:21 +0100

    reset ...

--- src/services/api/ErrorHandling.js
+++
@@ -1,8 +0,0 @@
-function handleFetchError(response) {
-    if (!response.ok && response.status !== 400) {
-        throw Error(response.statusText)
-    }
-    return response;
-}
-
-export { handleFetchError }
